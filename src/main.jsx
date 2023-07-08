c83dcb46f95c9ad62bf690d61693503d9c877cc8
hmpws <ttse007@aucklanduni.ac.nz>
2023-07-08 23:06:50 +1200

    removed more files

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "@shopify/polaris/build/esm/styles.css";
 import enTranslations from "@shopify/polaris/locales/en.json";
-import { AppProvider } from "@shopify/polaris";
+import { AppProvider, Page, LegacyCard, Button } from "@shopify/polaris";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <AppProvider i18n={enTranslations}>
-            <App />
+            <>
+                <Page title="Example app">
+                    <LegacyCard sectioned>
+                        <Button onClick={() => alert("Button clicked!")}>
+                            Example button
+                        </Button>
+                    </LegacyCard>
+                </Page>
+            </>
         </AppProvider>
     </React.StrictMode>
 );
