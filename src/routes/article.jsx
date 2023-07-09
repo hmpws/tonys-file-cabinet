c83b8f5e4b640c5db844f0a41839d886d455a342
hmpws <ttse007@aucklanduni.ac.nz>
2023-07-09 22:41:10 +1200

    rather not

--- src/routes/article.jsx
+++ src/routes/article.jsx
@@ -16,17 +16,17 @@ export async function loader({ request, params }) {
 }
 
 export default function Article() {
-    const { pathname } = useLocation();
+    // const { pathname } = useLocation();
 
-    useEffect(() => {
-        const canControlScrollRestoration =
-            "scrollRestoration" in window.history;
-        if (canControlScrollRestoration) {
-            window.history.scrollRestoration = "manual";
-        }
+    // useEffect(() => {
+    //     const canControlScrollRestoration =
+    //         "scrollRestoration" in window.history;
+    //     if (canControlScrollRestoration) {
+    //         window.history.scrollRestoration = "manual";
+    //     }
 
-        window.scrollTo(0, 0);
-    }, [pathname]);
+    //     window.scrollTo(0, 0);
+    // }, [pathname]);
 
     const { article } = useLoaderData();
     const [searchParams] = useSearchParams();
